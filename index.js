231ab090bd304e1b28087ea0b021b36b351c8bc0
sersanleo <sersanleo@alum.us.es>
2022-01-11 11:06:14 +0100

    Task-029: Pub/Sub implementation #41

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const app = require('./server.js');
+const Subscriptions = require("./subscriptions");
+const subscriptions = new Subscriptions();
 const dbConnect = require('./db');
+
+require("dotenv").config();
 
 var port = (process.env.PORT || 3000);
 
-console.log("Starting API server at "+port);
+console.log("Starting API server at " + port);
 
 dbConnect().then(
     () => {
+        subscriptions.initialize();
+        subscriptions.execute();
         app.listen(port);
         console.log("Server ready!");
     },
     err => {
-        console.log("Connection error: "+err);
+        console.log("Connection error: " + err);
     }
-) 
+)
