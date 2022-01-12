202dcb6112dd8064bd3406d20c9cac2b4888ac9c
sersanleo <sersanleo@alum.us.es>
2022-01-12 17:44:40 +0000

    Task-019: testing github actions deployment #21

---
+++ tests/server.test.js
@@ -0,0 +1,11 @@
+describe("Hello world tests", () => {
+
+    it("should do an stupid test", () => {
+        const a = 5;
+        const b = 3;
+
+        const sum = a + b;
+
+        expect(sum).toBe(8);
+    })
+})
